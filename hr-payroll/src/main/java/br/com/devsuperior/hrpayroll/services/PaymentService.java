af7a0bb0dca67607eb3e3101f2f1033ff447fb18
eliezermoraesss <eliezer.moraes@outlook.com>
2022-05-21 14:46:45 -0300

    RestTemplate

--- hr-payroll/src/main/java/br/com/devsuperior/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/br/com/devsuperior/hrpayroll/services/PaymentService.java
@@ -1,13 +1,30 @@
 package br.com.devsuperior.hrpayroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import br.com.devsuperior.hrpayroll.entities.Payment;
+import br.com.devsuperior.hrpayroll.entities.Worker;
 
 @Service
 public class PaymentService {
 	
+	@Value("${hr-worker.host}")
+	private String workerHost;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
 	public Payment getPayment(long workerId, int days) {
-		return new Payment("Eliezer", 3500.00, days);
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("id", ""+workerId);
+		
+		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }

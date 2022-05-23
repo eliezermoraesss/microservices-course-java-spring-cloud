24477c5e428c61fe8f56ac3bced351a96f5a8454
eliezermoraesss <eliezer.moraes@outlook.com>
2022-05-23 11:29:24 -0300

    Configuring Zuul api gateway

--- hr-payroll/src/main/java/br/com/devsuperior/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/br/com/devsuperior/hrpayroll/HrPayrollApplication.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SuppressWarnings("deprecation")
 @EnableCircuitBreaker
 @EnableEurekaClient
 @EnableFeignClients

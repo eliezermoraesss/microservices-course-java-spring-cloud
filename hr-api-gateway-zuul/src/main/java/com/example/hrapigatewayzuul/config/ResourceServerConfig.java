758bacfbf926d256d8b1b810543d6181607a320b
eliezermoraesss <eliezer.moraes@outlook.com>
2022-05-28 18:33:07 -0300

    Zuul authorization

---
+++ hr-api-gateway-zuul/src/main/java/com/example/hrapigatewayzuul/config/ResourceServerConfig.java
@@ -0,0 +1,41 @@
+package com.example.hrapigatewayzuul.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+	@Autowired
+	private JwtTokenStore tokenStore;
+	
+	private static final String[] PUBLIC = { "/hr-oauth/oauth/token" };
+	
+	private static final String[] OPERATOR = { "/hr-worker/**" };
+	
+	private static final String[] ADMIN = { "/hr-payroll/**", "/hr-user/**" };
+	
+	//Método que configura para receber o token
+	@Override
+	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+		resources.tokenStore(tokenStore);
+	}
+
+	//O método que vai configurar as autorizações
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+	
+		http.authorizeRequests()
+		.antMatchers(PUBLIC).permitAll()
+		.antMatchers(HttpMethod.GET, OPERATOR).hasAnyRole("OPERATOR", "ADMIN")
+		.antMatchers(ADMIN).hasRole("ADMIN")
+		.anyRequest().authenticated();
+	}
+}

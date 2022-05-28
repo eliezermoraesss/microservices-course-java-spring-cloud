d493651597c81000e9dc9dda8b581cca52d67b4f
eliezermoraesss <eliezer.moraes@outlook.com>
2022-05-28 15:55:03 -0300

    UserFeignClient

--- hr-oauth/src/main/java/br/com/devsuperior/hroauth/HrOauthApplication.java
+++ hr-oauth/src/main/java/br/com/devsuperior/hroauth/HrOauthApplication.java
@@ -3,7 +3,9 @@ package br.com.devsuperior.hroauth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @EnableEurekaClient
 @SpringBootApplication
 public class HrOauthApplication {
@@ -11,5 +13,4 @@ public class HrOauthApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HrOauthApplication.class, args);
 	}
-
 }
--- hr-oauth/src/main/java/br/com/devsuperior/hroauth/resources/UserResource.java
+++ hr-oauth/src/main/java/br/com/devsuperior/hroauth/resources/UserResource.java
@@ -1,34 +1,30 @@
-package br.com.devsuperior.hruser.resources;
+package br.com.devsuperior.hroauth.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.devsuperior.hruser.entities.User;
-import br.com.devsuperior.hruser.repositories.UserRepository;
+import br.com.devsuperior.hroauth.entities.User;
+import br.com.devsuperior.hroauth.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
-	
+
 	@Autowired
-	private UserRepository repository;
-	
-	@GetMapping(value = "/{id}")
-	public ResponseEntity<User> findById(@PathVariable Long id) {
-		
-		User entity = repository.findById(id).get();	
-		return ResponseEntity.ok(entity);
-	}
-	
+	private UserService userService;
+
 	@GetMapping(value = "/search")
 	public ResponseEntity<User> findByEmail(@RequestParam String email) {
-		
-		User entity = repository.findByEmail(email);	
-		return ResponseEntity.ok(entity);
-	} 
+		try {
+			User user = userService.findByEmail(email);
+			return ResponseEntity.ok(user);
+		} catch (IllegalArgumentException e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+	}
 }
